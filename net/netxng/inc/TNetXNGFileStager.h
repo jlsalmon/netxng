dfee8b816d4138f3ac6362003d338c4fe751b62a
Justin Salmon <jsalmon@cern.ch>
2013-04-30 14:10:23 +0200

    Document TNetXNGFileStager

--- net/netxng/inc/TNetXNGFileStager.h
+++ net/netxng/inc/TNetXNGFileStager.h
@@ -6,35 +6,95 @@
 #ifndef ROOT_TNetXNGFileStager
 #define ROOT_TNetXNGFileStager
 
-//------------------------------------------------------------------------------
-// Interface to the 'XRD' staging capabilities.
-//------------------------------------------------------------------------------
-
 #include "TFileStager.h"
 
 class TCollection;
 class TNetXNGSystem;
 class TFileCollection;
 
+//------------------------------------------------------------------------------
+// Interface to the 'XRD' staging capabilities.
+//------------------------------------------------------------------------------
 class TNetXNGFileStager: public TFileStager
 {
   public:
-    TNetXNGFileStager(const char *stager = "");
+    //--------------------------------------------------------------------------
+    //! Constructor
+    //!
+    //! @param url the URL of the entry-point server
+    //--------------------------------------------------------------------------
+    TNetXNGFileStager( const char *url = "" );
+
+    //--------------------------------------------------------------------------
+    //! Destructor
+    //--------------------------------------------------------------------------
     virtual ~TNetXNGFileStager();
 
-    Bool_t  IsStaged(const char *path);
-    Int_t   Locate(const char *path, TString &endpath);
-    Int_t   LocateCollection(TFileCollection *fc, Bool_t addDummyUrl = kFALSE);
-    Bool_t  Matches(const char *s);
-    Bool_t  Stage(const char *path, Option_t *opt = 0);
-    Bool_t  Stage(TCollection *pathlist, Option_t *opt = 0);
-    Bool_t  IsValid() const { return (fSystem ? kTRUE : kFALSE); }
-    void    Print(Option_t *option = "") const;
+    //--------------------------------------------------------------------------
+    //! Check if a file is ready to be used
+    //!
+    //! @param path the URL of the file
+    //--------------------------------------------------------------------------
+    Bool_t IsStaged( const char *path );
 
-   ClassDef( TNetXNGFileStager, 0 )  // Interface to a 'XRD' staging
+    //--------------------------------------------------------------------------
+    //! Get actual endpoint URL
+    //!
+    //! @param path    the entry-point URL
+    //! @param endpath the actual endpoint URL
+    //! @returns       0 in the case of success and 1 if any error occurred
+    //--------------------------------------------------------------------------
+    Int_t  Locate( const char *path, TString &endpath );
+
+    //--------------------------------------------------------------------------
+    //! Bulk locate request for a collection of files
+    //!
+    //! @param fc          collection of files to be located
+    //! @param addDummyUrl append a dummy noop URL if the file is not staged or
+    //!                    redirector == endpoint
+    //! @returns           < 0 in case of errors, number of files processed
+    //!                    otherwise
+    //--------------------------------------------------------------------------
+    Int_t  LocateCollection( TFileCollection *fc, Bool_t addDummyUrl = kFALSE );
+
+    //--------------------------------------------------------------------------
+    //! Returns kTRUE if stager 's' is compatible with current stager
+    //--------------------------------------------------------------------------
+    Bool_t Matches( const char *s );
+
+    //--------------------------------------------------------------------------
+    //! Issue a stage request for a single file
+    //!
+    //! @param path the path of the file to stage
+    //! @param opt  defines 'option' and 'priority' for 'Prepare': the format is
+    //!             opt = "option=o priority=p"
+    //--------------------------------------------------------------------------
+    Bool_t Stage( const char *path, Option_t *opt = 0 );
+
+    //--------------------------------------------------------------------------
+    //! Issue stage requests for multiple files
+    //!
+    //! @param pathlist list of paths of files to stage
+    //! @param opt      defines 'option' and 'priority' for 'Prepare': the
+    //!                 format is opt = "option=o priority=p"
+    //--------------------------------------------------------------------------
+    Bool_t Stage( TCollection *pathlist, Option_t *opt = 0 );
+
+    //--------------------------------------------------------------------------
+    //! Is this stager valid (instantiated)
+    //--------------------------------------------------------------------------
+    Bool_t IsValid() const { return ( fSystem ? kTRUE : kFALSE ); }
 
   private:
-    TNetXNGSystem   *fSystem;
+    //--------------------------------------------------------------------------
+    //! Data members
+    //--------------------------------------------------------------------------
+    TNetXNGSystem *fSystem;
+
+    //--------------------------------------------------------------------------
+    //! Interface to a 'XRD' staging
+    //--------------------------------------------------------------------------
+    ClassDef( TNetXNGFileStager, 0 )
 };
 
 #endif
--- net/netxng/src/TNetXNGFileStager.cxx
+++ net/netxng/src/TNetXNGFileStager.cxx
@@ -11,46 +11,71 @@
 //#include "TFileCollection.h"
 //#include "TStopwatch.h"
 //#include "TFileInfo.h"
+#include <iostream>
 
-TNetXNGFileStager::TNetXNGFileStager( const char */*url*/ ): TFileStager( "xrd" )
+ClassImp( TNetXNGFileStager );
+
+//------------------------------------------------------------------------------
+//! Constructor
+//------------------------------------------------------------------------------
+TNetXNGFileStager::TNetXNGFileStager( const char *url ):
+  TFileStager( "xrd" ), fSystem( url )
 {
+  std::cout << "Creating TNetXNGFileStager" << std::endl;
 }
 
+//------------------------------------------------------------------------------
+//! Destructor
+//------------------------------------------------------------------------------
 TNetXNGFileStager::~TNetXNGFileStager()
 {
 }
 
+//------------------------------------------------------------------------------
+//! Check if a file is ready to be used
+//------------------------------------------------------------------------------
 Bool_t TNetXNGFileStager::IsStaged( const char */*path*/ )
 {
   return kFALSE;
 }
 
+//------------------------------------------------------------------------------
+//! Get actual endpoint URL
+//------------------------------------------------------------------------------
+Int_t TNetXNGFileStager::Locate( const char */*path*/, TString &/*url*/ )
+{
+  return -1;
+}
+
+//------------------------------------------------------------------------------
+//! Bulk locate request for a collection of files
+//------------------------------------------------------------------------------
+Int_t TNetXNGFileStager::LocateCollection( TFileCollection */*fc*/,
+                                           Bool_t /*addDummyUrl*/ )
+{
+  return 0;
+}
+
+//------------------------------------------------------------------------------
+//! Returns kTRUE if stager 's' is compatible with current stager
+//------------------------------------------------------------------------------
+Bool_t TNetXNGFileStager::Matches( const char */*s*/ )
+{
+  return kFALSE;
+}
+
+//------------------------------------------------------------------------------
+//! Issue a stage request for a single file
+//------------------------------------------------------------------------------
 Bool_t TNetXNGFileStager::Stage( TCollection */*paths*/, Option_t */*opt*/ )
 {
   return kFALSE;
 }
 
+//------------------------------------------------------------------------------
+//! Issue stage requests for multiple files
+//------------------------------------------------------------------------------
 Bool_t TNetXNGFileStager::Stage( const char */*path*/, Option_t */*opt*/ )
-{
-  return kFALSE;
-}
-
-void TNetXNGFileStager::Print( Option_t * ) const
-{
-}
-
-Int_t TNetXNGFileStager::Locate( const char */*path*/, TString &/*eurl*/ )
-{
-   return -1;
-}
-
-Int_t TNetXNGFileStager::LocateCollection( TFileCollection */*fc*/,
-                                           Bool_t           /*addDummyUrl*/ )
-{
-  return 0;
-}
-
-Bool_t TNetXNGFileStager::Matches( const char */*s*/ )
 {
   return kFALSE;
 }
