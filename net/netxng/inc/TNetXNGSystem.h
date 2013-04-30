2e08a410c60e6534e170bb82184119610a1c23c1
Justin Salmon <jsalmon@cern.ch>
2013-04-30 13:25:53 +0200

    Document TNetXNGSystem

--- net/netxng/inc/TNetXNGSystem.h
+++ net/netxng/inc/TNetXNGSystem.h
@@ -8,24 +8,124 @@
 
 #include "TSystem.h"
 
+//------------------------------------------------------------------------------
+// Forward declarations
+//------------------------------------------------------------------------------
+namespace XrdCl
+{
+  class FileSystem;
+}
+
+//------------------------------------------------------------------------------
+//! Access system interface using the new client
+//------------------------------------------------------------------------------
 class TNetXNGSystem: public TSystem
 {
   public:
-    TNetXNGSystem(Bool_t owner = kTRUE);
-    TNetXNGSystem(const char *url, Bool_t owner = kTRUE);
-    virtual ~TNetXNGSystem() { }
+    //--------------------------------------------------------------------------
+    //! Constructor - Create system class without connecting to server
+    //!
+    //! @param owner (unused)
+    //--------------------------------------------------------------------------
+    TNetXNGSystem( Bool_t owner = kTRUE );
 
-    virtual Bool_t      ConsistentWith(const char *path, void *dirptr);
-    virtual void        FreeDirectory(void *dirp);
-    virtual const char *GetDirEntry(void *dirp);
-    virtual Int_t       GetPathInfo(const char* path, FileStat_t &buf);
-    virtual Bool_t      IsPathLocal(const char *path);
-    virtual Int_t       Locate(const char* path, TString &endurl);
-    virtual Int_t       MakeDirectory(const char* dir);
-    virtual void       *OpenDirectory(const char* dir);
-    virtual int         Unlink(const char *path);
+    //--------------------------------------------------------------------------
+    //! Constructor - Create system class and connect to server
+    //!
+    //! @param url   URL of the entry-point server to be contacted
+    //! @param owner (unused)
+    //--------------------------------------------------------------------------
+    TNetXNGSystem( const char *url, Bool_t owner = kTRUE );
 
-   ClassDef(TNetXNGSystem, 0 )
+    //--------------------------------------------------------------------------
+    //! Destructor
+    //--------------------------------------------------------------------------
+    virtual ~TNetXNGSystem() {}
+
+    //--------------------------------------------------------------------------
+    //! Open a directory
+    //!
+    //! @param dir the name of the directory to open
+    //! @returns   a non-zero pointer (with no special purpose) in case of
+    //!            success, 0 in case of error
+    //--------------------------------------------------------------------------
+    virtual void* OpenDirectory( const char* dir );
+
+    //--------------------------------------------------------------------------
+    //! Create a directory
+    //!
+    //! @param dir the directory name
+    //! @returns   0 on success, -1 otherwise
+    //--------------------------------------------------------------------------
+    virtual Int_t MakeDirectory( const char* dir );
+
+    //--------------------------------------------------------------------------
+    //! Free a directory
+    //!
+    //! @param dirp the pointer to the directory to be freed
+    //--------------------------------------------------------------------------
+    virtual void FreeDirectory( void *dirp );
+
+    //--------------------------------------------------------------------------
+    //! Get a directory entry.
+    //!
+    //! @param dirp the directory pointer
+    //! @returns    0 in case there are no more entries
+    //--------------------------------------------------------------------------
+    virtual const char* GetDirEntry( void *dirp );
+
+    //--------------------------------------------------------------------------
+    //! Get info about a file (stat)
+    //!
+    //! @param path the path of the file to stat (in)
+    //! @param buf  structure that will hold the stat info (out)
+    //! @returns    0 if success, 1 if the file could not be stat'ed
+    //--------------------------------------------------------------------------
+    virtual Int_t GetPathInfo( const char* path, FileStat_t &buf );
+
+    //--------------------------------------------------------------------------
+    //! Check consistency of this helper with the one required by 'path' or
+    //! 'dirptr'
+    //!
+    //! @param path   the path to check
+    //! @param dirptr the directory pointer to check
+    //--------------------------------------------------------------------------
+    virtual Bool_t ConsistentWith( const char *path, void *dirptr );
+
+    //--------------------------------------------------------------------------
+    //! Unlink a file on the remote server
+    //!
+    //! @param path the path of the file to unlink
+    //! @returns    0 on success, -1 otherwise
+    //--------------------------------------------------------------------------
+    virtual int Unlink( const char *path );
+
+    //--------------------------------------------------------------------------
+    //! Is this path a local path?
+    //!
+    //! @param path the URL of the path to check
+    //! @returns    kTRUE if the path is local, kFALSE otherwise
+    //--------------------------------------------------------------------------
+    virtual Bool_t IsPathLocal( const char *path );
+
+    //--------------------------------------------------------------------------
+    //! Get the endpoint URL of a file.
+    //!
+    //! @param path   the entry-point URL of the file (in)
+    //! @param endurl the endpoint URL of the file (out)
+    //--------------------------------------------------------------------------
+    virtual Int_t Locate( const char* path, TString &endurl );
+
+    //--------------------------------------------------------------------------
+    //! ROOT class definition
+    //--------------------------------------------------------------------------
+    ClassDef(TNetXNGSystem, 0 )
+
+  private:
+    //--------------------------------------------------------------------------
+    //! Data members
+    //--------------------------------------------------------------------------
+    XrdCl::FileSystem *fFileSystem;
 };
 
 #endif
--- net/netxng/src/TNetXNGSystem.cxx
+++ net/netxng/src/TNetXNGSystem.cxx
@@ -5,56 +5,96 @@
 
 #include "TNetXNGSystem.h"
 #include "Rtypes.h"
+#include <XrdCl/XrdClFileSystem.hh>
 
-ClassImp(TNetXNGSystem);
+ClassImp( TNetXNGSystem );
 
-TNetXNGSystem::TNetXNGSystem( Bool_t /*owner*/ )
+//------------------------------------------------------------------------------
+//! Constructor - Create system class without connecting to server
+//------------------------------------------------------------------------------
+TNetXNGSystem::TNetXNGSystem( Bool_t /*owner*/ ):
+  TSystem( "-root", "Net file Helper System" ), fFileSystem( 0 )
 {
+  using namespace XrdCl;
 }
 
-TNetXNGSystem::TNetXNGSystem( const char */*url*/, Bool_t /*owner*/)
+//------------------------------------------------------------------------------
+//! Constructor - Create system class and connect to server
+//------------------------------------------------------------------------------
+TNetXNGSystem::TNetXNGSystem( const char *url, Bool_t /*owner*/ ):
+  TSystem( "-root", "Net file Helper System" )
 {
+  using namespace XrdCl;
+  XrdCl::URL fileUrl( url );
+  fFileSystem = new XrdCl::FileSystem( fileUrl );
 }
 
-void *TNetXNGSystem::OpenDirectory( const char */*dir*/)
+//------------------------------------------------------------------------------
+//! Open a directory
+//------------------------------------------------------------------------------
+void *TNetXNGSystem::OpenDirectory( const char */*dir*/ )
 {
   return 0;
 }
 
-void TNetXNGSystem::FreeDirectory( void */*dirp*/)
-{
-}
-
+//------------------------------------------------------------------------------
+//! Create a directory
+//------------------------------------------------------------------------------
 Int_t TNetXNGSystem::MakeDirectory( const char */*dir*/ )
 {
   return -1;
 }
 
+//------------------------------------------------------------------------------
+//! Free a directory
+//------------------------------------------------------------------------------
+void TNetXNGSystem::FreeDirectory( void */*dirp*/ )
+{
+}
+
+//------------------------------------------------------------------------------
+//! Get a directory entry.
+//------------------------------------------------------------------------------
 const char *TNetXNGSystem::GetDirEntry( void */*dirp*/ )
 {
   return 0;
 }
 
+//------------------------------------------------------------------------------
+//! Get info about a file (stat)
+//------------------------------------------------------------------------------
 Int_t TNetXNGSystem::GetPathInfo( const char */*path*/, FileStat_t &/*buf*/ )
 {
   return -1;
 }
 
+//------------------------------------------------------------------------------
+//! Check consistency of this helper with the one required by 'path' or 'dirptr'
+//------------------------------------------------------------------------------
 Bool_t TNetXNGSystem::ConsistentWith( const char */*path*/, void */*dirptr*/ )
 {
   return kFALSE;
 }
 
+//------------------------------------------------------------------------------
+//! Unlink a file on the remote server
+//------------------------------------------------------------------------------
 int TNetXNGSystem::Unlink( const char */*path*/ )
 {
   return -1;
 }
 
+//------------------------------------------------------------------------------
+//! Is this path a local path?
+//------------------------------------------------------------------------------
 Bool_t TNetXNGSystem::IsPathLocal( const char */*path*/ )
 {
   return kFALSE;
 }
 
+//------------------------------------------------------------------------------
+//! Get the endpoint URL of a file.
+//------------------------------------------------------------------------------
 Int_t TNetXNGSystem::Locate( const char */*path*/, TString &/*eurl*/ )
 {
   return -1;
