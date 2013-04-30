6d94c642ef167b6a4939bfddb1c68a1fe6b2b516
Justin Salmon <jsalmon@cern.ch>
2013-04-30 11:27:28 +0200

    Document TNetXNGFile

--- net/netxng/inc/TNetXNGFile.h
+++ net/netxng/inc/TNetXNGFile.h
@@ -9,65 +9,144 @@
 #include "TFile.h"
 
 //------------------------------------------------------------------------------
-// Forward decls
+// Forward declarations
 //------------------------------------------------------------------------------
 namespace XrdCl
 {
   class File;
-};
+}
 
 //------------------------------------------------------------------------------
-// Access XRootD file using the new client
+//! Access XRootD file using the new client
 //------------------------------------------------------------------------------
 class TNetXNGFile: public TFile
 {
   public:
     //--------------------------------------------------------------------------
-    // Constructor
+    //! Constructor
     //--------------------------------------------------------------------------
     TNetXNGFile(): TFile(), fFile(0) {}
 
     //--------------------------------------------------------------------------
-    // Constructor
+    //! Constructor
+    //!
+    //! @param url          URL of the entry-point server to be contacted
+    //! @param mode         initial file access mode
+    //! @param title        title of the file (shown by ROOT browser)
+    //! @param compress     compression level and algorithm
+    //! @param netopt       TCP window size in bytes (do we need this?)
+    //! @param parallelopen open asynchronously (do we need this also?)
     //--------------------------------------------------------------------------
     TNetXNGFile( const char  *url,
-                 Option_t    *option       = "",
+                 Option_t    *mode         = "",
                  const char  *title        = "",
                  Int_t        compress     = 1,
                  Int_t        netopt       = 0,
                  Bool_t       parallelopen = kFALSE );
 
     //--------------------------------------------------------------------------
-    // Desctructor
+    //! Destructor
     //--------------------------------------------------------------------------
     virtual ~TNetXNGFile();
 
     //--------------------------------------------------------------------------
-    // 
+    //! Get the file size
     //--------------------------------------------------------------------------
-    virtual Long64_t    GetSize() const;
-    virtual Bool_t      IsOpen() const;
-    virtual void     Close(const Option_t *opt ="");
-    //virtual Int_t       ReOpen(Option_t *mode);
-    virtual Bool_t   ReadBuffer( char *buf, Int_t len );
-    virtual Bool_t   ReadBuffer( char *buf, Long64_t pos, Int_t len );
-    virtual Bool_t   ReadBuffers(char *buf,  Long64_t *pos, Int_t *len, Int_t nbuf);
-//    virtual Bool_t   WriteBuffer(const char *buffer, Int_t BufferLength);
-    virtual void        Seek(Long64_t offset, ERelativeTo pos = kBeg);
-    ClassDef( TNetXNGFile,0 )
+    virtual Long64_t GetSize() const;
+
+    //--------------------------------------------------------------------------
+    //! Is the file open?
+    //--------------------------------------------------------------------------
+    virtual Bool_t IsOpen() const;
+
+    //--------------------------------------------------------------------------
+    //! Close the file
+    //!
+    //! @param option if == "R", all TProcessIDs referenced by this file are
+    //!               deleted (is this valid in xrootd context?)
+    //--------------------------------------------------------------------------
+    virtual void Close(const Option_t *option = "");
+
+    //--------------------------------------------------------------------------
+    //! Reopen the file with the new access mode
+    //!
+    //! @param mode the new access mode
+    //! @returns    0 in case the mode was successfully modified, 1 in case
+    //!             the mode did not change (was already as requested or wrong
+    //!             input arguments) and -1 in case of failure, in which case
+    //!             the file cannot be used anymore
+    //--------------------------------------------------------------------------
+    virtual Int_t ReOpen(Option_t *mode);
+
+    //--------------------------------------------------------------------------
+    //! Read a data chunk of the given size
+    //!
+    //! @param buffer a pointer to a buffer big enough to hold the data
+    //! @param length number of bytes to be read
+    //! @returns      kTRUE in case of failure
+    //--------------------------------------------------------------------------
+    virtual Bool_t ReadBuffer( char *buffer, Int_t length );
+
+    //--------------------------------------------------------------------------
+    //! Read a data chunk of the given size, starting from the given offset
+    //!
+    //! @param buffer   a pointer to a buffer big enough to hold the data
+    //! @param position offset from the beginning of the file
+    //! @param length   number of bytes to be read
+    //! @returns        kTRUE in case of failure
+    //--------------------------------------------------------------------------
+    virtual Bool_t ReadBuffer( char *buffer, Long64_t position, Int_t length );
+
+    //--------------------------------------------------------------------------
+    //! Read scattered data chunks in one operation
+    //!
+    //! @param buffer   a pointer to a buffer big enough to hold all of the
+    //!                 requested data
+    //! @param position position[i] is the seek position of chunk i of len
+    //!                 length[i]
+    //! @param length   length[i] is the length of the chunk at offset
+    //!                 position[i]
+    //! @param nbuffs   number of chunks
+    //! @returns        kTRUE in case of failure
+    //--------------------------------------------------------------------------
+    virtual Bool_t ReadBuffers(char     *buffer,
+                               Long64_t *position,
+                               Int_t    *length,
+                               Int_t     nbuffs);
+
+    //--------------------------------------------------------------------------
+    //! Write a data chunk
+    //!
+    //! @param buffer the data to be written
+    //! @param length the size of the buffer
+    //! @returns      kTRUE in case of failure
+    //--------------------------------------------------------------------------
+    virtual Bool_t WriteBuffer(const char *buffer, Int_t length);
+
+    //--------------------------------------------------------------------------
+    //! Set the position within the file
+    //!
+    //! @param offset   the new offset relative to position
+    //! @param position the relative position, either kBeg, kCur or kEnd
+    //--------------------------------------------------------------------------
+    virtual void Seek(Long64_t offset, ERelativeTo position = kBeg);
+
+    //--------------------------------------------------------------------------
+    //! ROOT class definition
+    //--------------------------------------------------------------------------
+    ClassDef( TNetXNGFile, 0 )
 
   private:
     //--------------------------------------------------------------------------
-    // Data members
+    //! Data members
     //--------------------------------------------------------------------------
     XrdCl::File *fFile;
 
     //--------------------------------------------------------------------------
-    // 
+    //! The file is not copyable
     //--------------------------------------------------------------------------
     TNetXNGFile( const TNetXNGFile &other );
     TNetXNGFile &operator = (const TNetXNGFile &other );
-
 };
 
 #endif // ROOT_TNetXNGFile
--- net/netxng/src/TNetXNGFile.cxx
+++ net/netxng/src/TNetXNGFile.cxx
@@ -11,10 +11,10 @@
 ClassImp(TNetXNGFile);
 
 //------------------------------------------------------------------------------
-// Constructor
+//! Constructor
 //------------------------------------------------------------------------------
 TNetXNGFile::TNetXNGFile( const char  *url,
-                          Option_t    */*option*/,
+                          Option_t    */*mode*/,
                           const char  *title,
                           Int_t        compress,
                           Int_t        /*netopt*/,
@@ -26,12 +26,12 @@ TNetXNGFile::TNetXNGFile( const char  *url,
   fFile = new XrdCl::File();
   XrdCl::URL fileUrl( url );
   fileUrl.SetProtocol( "root" );
-  fFile->Open( fileUrl.GetURL().c_str(), OpenFlags::Read );  
+  fFile->Open( fileUrl.GetURL().c_str(), OpenFlags::Read );
   TFile::Init( false );
 }
 
 //------------------------------------------------------------------------------
-// Desctructor
+//! Destructor
 //------------------------------------------------------------------------------
 TNetXNGFile::~TNetXNGFile()
 {
@@ -40,6 +40,9 @@ TNetXNGFile::~TNetXNGFile()
   delete fFile;
 }
 
+//------------------------------------------------------------------------------
+//! Get the file size
+//------------------------------------------------------------------------------
 Long64_t TNetXNGFile::GetSize() const
 {
   using namespace XrdCl;
@@ -47,47 +50,85 @@ Long64_t TNetXNGFile::GetSize() const
   fFile->Stat( false, info );
   Long64_t size = info->GetSize();
   delete info;
-//  std::cout << "size: " << size << std::endl;
   return size;
 }
 
+//------------------------------------------------------------------------------
+//! Is the file open?
+//------------------------------------------------------------------------------
 Bool_t TNetXNGFile::IsOpen() const
 {
   return fFile->IsOpen();
 }
 
-Bool_t TNetXNGFile::ReadBuffer( char *buf, Int_t len )
+//------------------------------------------------------------------------------
+//! Close the file
+//------------------------------------------------------------------------------
+void TNetXNGFile::Close( const Option_t */*option*/)
+{
+  fFile->Close();
+}
+
+//------------------------------------------------------------------------------
+//! Reopen the file with the new access mode
+//------------------------------------------------------------------------------
+Int_t TNetXNGFile::ReOpen(Option_t */*mode*/)
+{
+  return 0;
+}
+
+//------------------------------------------------------------------------------
+//! Read a data chunk of the given size
+//------------------------------------------------------------------------------
+Bool_t TNetXNGFile::ReadBuffer( char *buffer, Int_t length )
 {
   using namespace XrdCl;
   uint32_t bytesRead = 0;
-//  std::cout << "rb 1 o " << fOffset << " l " << len << std::endl;
-  XRootDStatus st = fFile->Read( fOffset, len, buf, bytesRead );
-//  std::cout << "[x]" << st.ToStr() << " " << bytesRead << std::endl;
+  std::cout << "rb 1 o " << fOffset << " l " << length << std::endl;
+  XRootDStatus st = fFile->Read( fOffset, length, buffer, bytesRead );
+  std::cout << "[x]" << st.ToStr() << " " << bytesRead << std::endl;
   fOffset += bytesRead;
   return false;
 }
 
-Bool_t TNetXNGFile::ReadBuffer( char *buf, Long64_t pos, Int_t len )
+//------------------------------------------------------------------------------
+//! Read a data chunk of the given size, starting from the given offset
+//------------------------------------------------------------------------------
+Bool_t TNetXNGFile::ReadBuffer( char *buffer, Long64_t position, Int_t length )
 {
   using namespace XrdCl;
-//  std::cout << "rb 2" << std::endl;
+  std::cout << "rb 2" << std::endl;
   uint32_t bytesRead = 0;
-  XRootDStatus st = fFile->Read( pos, len, buf, bytesRead );
+  XRootDStatus st = fFile->Read( position, length, buffer, bytesRead );
   return true;
 }
 
-Bool_t TNetXNGFile::ReadBuffers(char *buf,  Long64_t *pos, Int_t *len, Int_t nbuf)
+//------------------------------------------------------------------------------
+//! Read scattered data chunks in one operation
+//------------------------------------------------------------------------------
+Bool_t TNetXNGFile::ReadBuffers( char     *buffer,
+                                 Long64_t *position,
+                                 Int_t    *length,
+                                 Int_t     nbuffs )
 {
-//  std::cout << "rb3 " << std::endl;
+  std::cout << "rb3 " << std::endl;
   return true;
 }
 
-void TNetXNGFile::Seek(Long64_t offset, ERelativeTo pos)
+//------------------------------------------------------------------------------
+//! Write a data chunk
+//------------------------------------------------------------------------------
+Bool_t TNetXNGFile::WriteBuffer(const char *buffer, Int_t length)
 {
-  SetOffset(offset, pos);
+  return false;
 }
 
-void TNetXNGFile::Close(const Option_t */*opt*/)
+//------------------------------------------------------------------------------
+//! Set the position within the file
+//------------------------------------------------------------------------------
+void TNetXNGFile::Seek( Long64_t offset, ERelativeTo position )
 {
-  fFile->Close();
+  SetOffset( offset, position );
 }
+
+
