b6437e5caabeb01e24ab646bad1bc5b8262b0208
Justin Salmon <jsalmon@cern.ch>
2013-05-08 09:56:20 +0200

    Implement GetPathInfo()

--- net/netxng/src/TNetXNGSystem.cxx
+++ net/netxng/src/TNetXNGSystem.cxx
@@ -6,6 +6,7 @@
 #include "TNetXNGSystem.h"
 #include "Rtypes.h"
 #include <XrdCl/XrdClFileSystem.hh>
+#include <XrdCl/XrdClXRootDResponses.hh>
 
 ClassImp( TNetXNGSystem );
 
@@ -15,7 +16,7 @@ ClassImp( TNetXNGSystem );
 TNetXNGSystem::TNetXNGSystem( Bool_t /*owner*/ ):
   TSystem( "-root", "Net file Helper System" ), fFileSystem( 0 )
 {
-  using namespace XrdCl;
+  Info( "TNetXNGSystem", "Creating TNetXNGSystem" );
 }
 
 //------------------------------------------------------------------------------
@@ -25,8 +26,9 @@ TNetXNGSystem::TNetXNGSystem( const char *url, Bool_t /*owner*/ ):
   TSystem( "-root", "Net file Helper System" )
 {
   using namespace XrdCl;
-  XrdCl::URL fileUrl( url );
-  fFileSystem = new XrdCl::FileSystem( fileUrl );
+  Info( "TNetXNGSystem", "Creating TNetXNGSystem" );
+  URL fileUrl( url );
+  fFileSystem = new FileSystem( fileUrl );
 }
 
 //------------------------------------------------------------------------------
@@ -34,7 +36,8 @@ TNetXNGSystem::TNetXNGSystem( const char *url, Bool_t /*owner*/ ):
 //------------------------------------------------------------------------------
 void *TNetXNGSystem::OpenDirectory( const char */*dir*/ )
 {
-  return 0;
+  Info( "TNetXNGSystem", "OpenDirectory()" );
+  return (void *) 1;
 }
 
 //------------------------------------------------------------------------------
@@ -63,9 +66,55 @@ const char *TNetXNGSystem::GetDirEntry( void */*dirp*/ )
 //------------------------------------------------------------------------------
 //! Get info about a file (stat)
 //------------------------------------------------------------------------------
-Int_t TNetXNGSystem::GetPathInfo( const char */*path*/, FileStat_t &/*buf*/ )
+Int_t TNetXNGSystem::GetPathInfo( const char *path, FileStat_t &buf )
 {
-  return -1;
+  Info( "TNetXNGSystem", "GetPathInfo()" );
+  using namespace XrdCl;
+  StatInfo *info = 0;
+  URL target( path );
+  XRootDStatus st = fFileSystem->Stat( target.GetPath(), info );
+
+  if( !st.IsOK() )
+  {
+    Error( "GetPathInfo", "%s", st.GetErrorMessage().c_str() );
+    delete info;
+    return 1;
+  }
+  else
+  {
+    //--------------------------------------------------------------------------
+    // Flag offline files
+    //--------------------------------------------------------------------------
+    if ( info->GetFlags() & kXR_offline )
+    {
+      buf.fMode = kS_IFOFF;
+    }
+    else
+    {
+      std::stringstream sstr( info->GetId() );
+      Long64_t id;
+      sstr >> id;
+
+      buf.fDev    = ( id >> 32 );
+      buf.fIno    = ( id & 0x00000000FFFFFFFF );
+      buf.fUid    = -1;  // not available
+      buf.fGid    = -1;  // not available
+      buf.fIsLink = 0;   // not available
+      buf.fSize   = info->GetSize();
+      buf.fMtime  = info->GetModTime();
+
+      if ( info->GetFlags() & kXR_xset )
+        buf.fMode = ( kS_IFREG | kS_IXUSR | kS_IXGRP | kS_IXOTH );
+      if ( info->GetFlags() == 0 )           buf.fMode  = kS_IFREG;
+      if ( info->GetFlags() & kXR_isDir )    buf.fMode  = kS_IFDIR;
+      if ( info->GetFlags() & kXR_other )    buf.fMode  = kS_IFSOCK;
+      if ( info->GetFlags() & kXR_readable ) buf.fMode |= kS_IRUSR;
+      if ( info->GetFlags() & kXR_writable ) buf.fMode |= kS_IWUSR;
+    }
+  }
+
+  delete info;
+  return 0;
 }
 
 //------------------------------------------------------------------------------
